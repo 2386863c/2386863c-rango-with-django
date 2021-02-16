1759469e37a73b41da1f333b117f3857d8f019aa
2386863c <78848675+2386863c@users.noreply.github.com>
2021-02-17 01:17:25 +0200

    Chapter 10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -17,51 +17,63 @@ from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 def index(request):
-# Query the database for a list of ALL categories currently stored.
-# Order the categories by the number of likes in descending order.
-# Retrieve the top 5 only -- or all if less than 5.
-# Place the list in our context_dict dictionary (with our boldmessage!)
-# that will be passed to the template engine.
+    #Test cookie
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories']= category_list
     context_dict['pages']= pages_list
 
-# Render the response and send it back!
-    return render(request,'rango/index.html', context=context_dict)
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+
+    # Return response back to the user, updating any cookies that need changed.
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    return render(request,'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request,'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-# Create a context dictionary which we can pass
-# to the template rendering engine.
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
     context_dict={}
     try:
-    # Can we find a category name slug with the given name?
-    # If we can't, the .get() method raises a DoesNotExist exception.
-    # The .get() method returns one model instance or raises an exception.
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # The .get() method returns one model instance or raises an exception.
         category=Category.objects.get(slug=category_name_slug)
 
-    # Retrieve all of the associated pages.
-    # The filter() will return a list of page objects or an empty list.
+        # Retrieve all of the associated pages.
+        # The filter() will return a list of page objects or an empty list.
         pages=Page.objects.filter(category=category)
 
-    # Adds our results list to the template context under name pages.
+        # Adds our results list to the template context under name pages.
         context_dict['pages']=pages
 
-    # We also add the category object from
-    # the database to the context dictionary.
-    # We'll use this in the template to verify that the category exists.
+        # We also add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
         context_dict['category']=category
 
     except Category.DoesNotExist:
-    # We get here if we didn't find the specified category.
-    # Don't do anything -
-    # the template will display the "no category" message for us.
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.
         context_dict['category']= None
         context_dict['pages']=None
 
@@ -234,3 +246,28 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val=request.session.get(cookie)
+    if not val:
+        val=default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits=int(get_server_side_cookie(request,'visits','1'))
+    last_visit_cookie=get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time=datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if(datetime.now()-last_visit_time).days>0:
+        visits=visits+1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit']=str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit']=last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits']=visits
